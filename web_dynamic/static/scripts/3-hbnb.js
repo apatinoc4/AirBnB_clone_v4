2da61ff6c9f04dee4a84e975d9feecd3e6205ade
apatinoc4 <apatinoc4@gmail.com>
2020-02-17 21:58:55 -0500

    test

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-  const idDict = {};
+  let idDict = {};
   $('input:checkbox').change(function () {
     if ($(this).is(':checked')) {
       idDict[$(this).attr('data-name')] = $(this).attr('data-id');
     } else {
       delete idDict[$(this).attr('data-name')];
     }
-    const list = [];
+    let list = [];
     $.each(idDict, function (index, value) {
       list.push(index);
     });
@@ -18,7 +18,7 @@ $(document).ready(function () {
   });
 
   $.get('http://0.0.0.0:5001/api/v1/status', function (data) {
-    if (data.status === 'OK') {
+    if (data['status'] === 'OK') {
       $('DIV#api_status').addClass('available');
     } else {
       $('DIV#api_status').removeClass('available');
@@ -35,38 +35,38 @@ $(document).ready(function () {
   });
 
   function createPlaces (data) {
-    const places = [];
+    let places = [];
     console.log(data);
-    for (const i in data) {
+    for (let i in data) {
       const name = data[i].name;
       const price = data[i].price_by_night;
       const desc = data[i].description;
       const guest = data[i].max_guest;
       const bed = data[i].number_rooms;
       const bath = data[i].number_bathrooms;
-      const place = `
-        <ARTICLE>
-           <div class="title">
-         <h2>${name}</h2>
-         <div class="price_by_night">$${price}</div>
+      let place = `
+      <ARTICLE>
+         <div class="title">
+	   <h2>${name}</h2>
+	   <div class="price_by_night">$${price}</div>
+         </div>
+	 <div class="information">
+	   <div class="max_guest">
+             <i class="fa fa-users fa-3x" aria-hidden="true"></i>
+             <br>${guest} ${guest > 1 ? 'Guests' : 'Guest'}
            </div>
-       <div class="information">
-         <div class="max_guest">
-               <i class="fa fa-users fa-3x" aria-hidden="true"></i>
-               <br>${guest} ${guest > 1 ? 'Guests' : 'Guest'}
-             </div>
-             <div class="number_rooms">
-           <i class="fa fa-bed fa-3x" aria-hidden="true"></i>
-               <br>${bed} ${bed > 1 ? 'Bedrooms' : 'Bedroom'}            
-             </div>
-             <div class="number_bathrooms">
-               <i class="fa fa-bath fa-3x" aria-hidden="true"></i>
-               <br>${bath} ${bath > 1 ? 'Bathrooms' : 'Bathroom'}
-             </div>
-       </div>
-           <div class="description">${desc}</div>
-        </ARTICLE>
-        `;
+           <div class="number_rooms">
+	     <i class="fa fa-bed fa-3x" aria-hidden="true"></i>
+             <br>${bed} ${bed > 1 ? 'Bedrooms' : 'Bedroom'}            
+           </div>
+           <div class="number_bathrooms">
+             <i class="fa fa-bath fa-3x" aria-hidden="true"></i>
+             <br>${bath} ${bath > 1 ? 'Bathrooms' : 'Bathroom'}
+           </div>
+	 </div>
+         <div class="description">${desc}</div>
+      </ARTICLE>
+      `;
       places.push(place);
     }
     $('.places').append(places);
